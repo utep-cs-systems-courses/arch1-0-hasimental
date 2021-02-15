53e506d3be2843f5177c6698cbc31c841a7f7729
student <hasimental@miners.utep.edu>
2021-02-14 22:58:30 -0700

    edit to the ui and draw_shapes and added what needed to be added to the draw.h file

--- draw_shapes.c
+++ draw_shapes.c
@@ -25,4 +25,23 @@ void print_triangle(int leftCol, int size)
     putchar('\n');
   }
 }
+// this will attempt to print the arrow 
+void print_arrow(int leftCol, int size)
+{
+ int endCol = leftCol + size;
+  for (int row = 0; row < size; row++){
+    int col;
+    for (col = 0; col < leftCol; col++) putchar(' ');
+    for (       ; col < endCol;  col++) putchar('*');
+    putchar('\n');
+  }
+  
+  for (int row = 0; row <= size; row++) {
+    int minCol = leftCol + size - row, maxCol = leftCol + size + row;
+    int col;
+    for (col = 0; col < minCol; col++) putchar(' ');
+    for (       ; col <= maxCol; col++) putchar('*');
+    putchar('\n');
+ }
 
+}
